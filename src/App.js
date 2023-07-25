ea3661b6ac21d8070c17ce0328e3a78e8296d540
Arjun <arjuns231@gmail.com>
2023-07-25 14:08:54 +0530

    addition feature added - for edit todo list

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React, { useState, useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faEdit, faSave } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-
   const [todos, setTodos] = useState([]);
   const [todoText, setTodoText] = useState('');
+  const [editModeIndex, setEditModeIndex] = useState(-1);
+  const [editText, setEditText] = useState(''); // New state for temporary edit text
 
   const handleAddTodo = (event) => {
     event.preventDefault();
@@ -29,6 +30,18 @@ function App() {
     setTodos(newTodos);
   };
 
+  const handleEditTodo = (index) => {
+    setEditModeIndex(index);
+    setEditText(todos[index].text); // Initialize the temporary edit text with the current todo text
+  };
+
+  const handleSaveTodo = (index, newText) => {
+    const newTodos = [...todos];
+    newTodos[index].text = newText;
+    setTodos(newTodos);
+    setEditModeIndex(-1); // Turn off edit mode after saving
+  };
+
   const getCompletedTodoCount = () => {
     return todos.filter((todo) => todo.completed).length;
   };
@@ -37,8 +50,8 @@ function App() {
     return todos.length;
   };
 
-   // Load todos from local storage on component mount
-   useEffect(() => {
+  // Load todos from local storage on component mount
+  useEffect(() => {
     const storedTodos = localStorage.getItem('todos');
     if (storedTodos) {
       setTodos(JSON.parse(storedTodos));
@@ -52,43 +65,68 @@ function App() {
 
   return (
     <>
-      <div className='todohead'><h1>Todo App</h1>  Completed: {getCompletedTodoCount()}/{getTotalTodoCount()} </div>
-      
+      <div className='todohead'>
+        <h1>Todo App</h1> Completed: {getCompletedTodoCount()}/{getTotalTodoCount()}{' '}
+      </div>
+
       <form onSubmit={handleAddTodo}>
-      <button type="submit">  <FontAwesomeIcon icon={faPlus} style={{ marginRight: '5px' }} /></button>
+        <button type='submit'>
+          <FontAwesomeIcon icon={faPlus} style={{ marginRight: '5px' }} />
+        </button>
         <input
-          type="text"
+          type='text'
           value={todoText}
           onChange={(event) => setTodoText(event.target.value)}
-          placeholder="Add a todo..."
+          placeholder='Add a todo...'
         />
       </form>
       {todos.length === 0 ? (
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
           <label style={{ textAlign: 'center' }}>Add your First todo:)</label>
-        <hr style={{ flexGrow: 1, margin: '5px 0', border: '0.5px solid #ccc' }} />
-      </div>      
+          <hr style={{ flexGrow: 1, margin: '5px 0', border: '0.5px solid #ccc' }} />
+        </div>
       ) : (
-      <ul>
-        {todos.map((todo, index) => (
-          <div>
-            <li key={index} style={{ display: 'flex', alignItems: 'center' }}>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              onChange={() => handleToggleCompleted(index)}
-            />
-            <label style={{ textDecoration: todo.completed ? 'line-through' : 'none', flexGrow: 1 }}>
-              {todo.text}
-            </label>
-              {/* <button> */}
-              <FontAwesomeIcon icon={faTrash} style={{ color: 'red' }}  onClick={() => handleDeleteTodo(index)} />
-              {/* </button> */}
-          </li>
+        <ul>
+          {todos.map((todo, index) => (
+            <div key={index}>
+              <li style={{ display: 'flex', alignItems: 'center' }}>
+                {editModeIndex === index ? (
+                  <>
+                    <input
+                      type='text'
+                      value={editText}
+                      onChange={(event) => setEditText(event.target.value)}
+                      autoFocus // Automatically focus on the input when in edit mode
+                    />
+                    <button onClick={() => handleSaveTodo(index, editText)}>
+                      <FontAwesomeIcon icon={faSave} style={{ color: 'green' }} />
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <input
+                      type='checkbox'
+                      checked={todo.completed}
+                      onChange={() => handleToggleCompleted(index)}
+                    />
+                    <label
+                      style={{ textDecoration: todo.completed ? 'line-through' : 'none', flexGrow: 1 }}
+                    >
+                      {todo.text}
+                    </label>
+                    <button onClick={() => handleEditTodo(index)}>
+                      <FontAwesomeIcon icon={faEdit} style={{ color: 'blue' }} />
+                    </button>
+                    <button onClick={() => handleDeleteTodo(index)}>
+                      <FontAwesomeIcon icon={faTrash} style={{ color: 'red' }} />
+                    </button>
+                  </>
+                )}
+              </li>
               <hr style={{ flexGrow: 1, margin: '5px 0', border: '0.5px solid #ccc' }} />
-          </div>
-        ))}
-      </ul>
+            </div>
+          ))}
+        </ul>
       )}
     </>
   );
